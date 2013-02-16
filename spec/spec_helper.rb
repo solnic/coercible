d5bb18fa1e9fb8ec368739e3a0f674e46cecbf80
Piotr Solnica <piotr.solnica@gmail.com>
2013-02-16 18:33:13 +0100

    Fix coverage setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,15 @@
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter "/spec/"
+  end
+end
+
 require 'coercible'
 
 include Coercible
 
 ENV['TZ'] = 'UTC'
-
-if ENV['COVERAGE']
-  require 'simplecov'
-  SimpleCov.start
-end
 
 Dir[File.expand_path('../shared/**/*.rb', __FILE__)].each { |file| require file }
 
