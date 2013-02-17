30b74e7a754739b246f41cecd5e7e76d4c51520a
Piotr Solnica <piotr.solnica@gmail.com>
2013-02-17 12:35:22 +0100

    Fix Float/Integer to_datetime specs for 1.8.7

--- spec/unit/coercible/coercer/integer/to_datetime_spec.rb
+++ spec/unit/coercible/coercer/integer/to_datetime_spec.rb
@@ -6,12 +6,7 @@ describe Coercer::Integer, '#to_datetime' do
   let(:object) { described_class.new }
   let(:value)  { 1361036672 }
 
-  it { should be_instance_of(DateTime) }
-
-  its(:year)   { should be(2013) }
-  its(:month)  { should be(2) }
-  its(:day)    { should be(16) }
-  its(:hour)   { should be(17) }
-  its(:minute) { should be(44) }
-  its(:second) { should be(32) }
+  specify do
+    expect(subject.strftime('%Y-%m-%d %H:%M:%S.%L')).to eql('2013-02-16 17:44:32.000')
+  end
 end
