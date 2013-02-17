3c0de11f50744a03bd9114c1f711e9e0139a6cc8
Piotr Solnica <piotr.solnica@gmail.com>
2013-02-17 11:32:19 +0100

    Improve Float/Integer to_datetime specs

--- spec/unit/coercible/coercer/float/to_datetime_spec.rb
+++ spec/unit/coercible/coercer/float/to_datetime_spec.rb
@@ -7,4 +7,11 @@ describe Coercer::Float, '#to_float' do
   let(:value)  { 1361036672.12 }
 
   it { should be_instance_of(DateTime) }
+
+  its(:year)   { should be(2013) }
+  its(:month)  { should be(2) }
+  its(:day)    { should be(16) }
+  its(:hour)   { should be(17) }
+  its(:minute) { should be(44) }
+  its(:second) { should be(32) }
 end
